a570aadb1f9b968399017adfd3de44f3cf8911dc
Sena Heydari <sheydari@dosomething.org>
2016-11-22 15:59:38 -0500

    Reverting to MySQL connector for Quasar.

--- etl-scripts/mailchimp_subscribers.py
+++ etl-scripts/mailchimp_subscribers.py
@@ -5,7 +5,8 @@ import time
 import datetime
 import csv
 from subprocess import Popen, PIPE, STDOUT
-import psycopg2
+import MySQLdb
+import MySQLdb.converters
 import sys
 import config
 
@@ -24,66 +25,52 @@ time_now = time.time()
 backfill_time = backfill_hours * 3600
 origin_time = time_now - backfill_time
 
-# Set initial time Windows from current time
-start_window_time = time_now - 7200
-end_window_time = time_now
-
 # MailChimp API v3.0 HTTP Simple Auth Credentials
 un = config.mailchimp_api_user
 pw = config.mailchimp_api_pass
 
-# MailChimp API v3.0 Paremeters to Get Unsubscribed Users over 2 hour time window assuming max 10000 subscribers per time windows. Number guidelines
-# set via conversations with Dee on max ingestion rate
-info = {'status':'subscribed', 'since_timestamp_opt':datetime.datetime.utcfromtimestamp(start_window_time).isoformat() , 'before_timestamp_opt':datetime.datetime.utcfromtimestamp(end_window_time).isoformat() , 'count':10000, 'fields':'members.email_address,members.last_changed'}
+# Set Initial Offset of O
+member_offset = 0
+
+# MailChimp API v3.0 Paremeters to Get Subscribed Users over designated time window by paginating via "offset" parameter.
+# Grabs in batches of 1000 to be nice to API. Can modify based on query times.
+info = {'status':'subscribed', 'since_timestamp_opt':datetime.datetime.utcfromtimestamp(origin_time).isoformat() , 'before_timestamp_opt':datetime.datetime.utcfromtimestamp(time_now).isoformat() , 'count':1000, 'fields':'members.email_address,members.last_changed', 'offset':member_offset}
 
 # Initialize Empty List for Total Members
 total_members = []
 
-# Iterate over 2 hour time windows to build out large member array of unsubscribes
-while (start_window_time - origin_time) >= -7200:
+# Get first batch of users
+r = requests.get('https://us4.api.mailchimp.com/3.0/lists/f2fab1dfd4/members',auth=HTTPBasicAuth(un, pw), params=info)
+member_array = r.json()
+
+# Iterate over entire list set till no more members are returned.
+while (len(member_array['members'])) > 1:
+    total_members.append(member_array['members'])
+    print("Total Members in Array is currently %s" % len(total_members))
+    member_offset += 1000
+    info = {'status':'subscribed', 'since_timestamp_opt':datetime.datetime.utcfromtimestamp(origin_time).isoformat() , 'before_timestamp_opt':datetime.datetime.utcfromtimestamp(time_now).isoformat() , 'count':1000, 'fields':'members.email_address,members.last_changed', 'offset':member_offset}
     r = requests.get('https://us4.api.mailchimp.com/3.0/lists/f2fab1dfd4/members',auth=HTTPBasicAuth(un, pw), params=info)
     member_array = r.json()
-    if not member_array['members']:
-        print("No subscribes for this time window!")
-    else:
-        total_members.append(member_array['members'])
-        print("Total Members in Array is currently %s" % len(total_members))
-    end_window_time = start_window_time
-    start_window_time -= 7200
-    info = {'status':'subscribed', 'since_timestamp_opt':datetime.datetime.utcfromtimestamp(start_window_time).isoformat() , 'before_timestamp_opt':datetime.datetime.utcfromtimestamp(end_window_time).isoformat() , 'count':10000, 'fields':'members.email_address,members.last_changed'}
 
-db = psycopg2.connect(
-          user=config.user,
-          password=config.pw,
-          dbname=config.db,
-          host=config.host,
-          port="5439",
-          sslmode="require")
+# Setup DB Connnection
+conv_dict = MySQLdb.converters.conversions.copy()
+conv_dict[246]=float
+conv_dict[3]=int
+
+db = MySQLdb.connect(host=config.host, #hostname
+          user=config.user, #  username
+          passwd=config.pw, #  password
+          conv=conv_dict) # datatype conversions
 
 cur = db.cursor()
-
-# Create staging table for changes
-cur.execute("DROP TABLE IF EXISTS users_and_activities.mailchimp_sub_staging")
-db.commit()
-cur.execute("CREATE TABLE users_and_activities.mailchimp_sub_staging (like users_and_activities.mailchimp_sub)")
-db.commit()
 
 # Iterate over entire member array and add to DB
 for member in total_members:
     for submember in member:
         email = submember['email_address']
         confirm_time = submember['last_changed'].replace('T',' ').split('+')[0]
-        cur.execute("INSERT into users_and_activities.mailchimp_sub_staging values (%s,%s)", (email,confirm_time))
+        cur.execute("INSERT IGNORE INTO users_and_activities.mailchimp_sub (email_address, confirm_time) VALUES(%s,%s)", (email,confirm_time))
 
-db.commit()
-
-# Clean-Up and Remove Duplicates - Placeholder workaround since RedShift doesn't strictly enforce Primary Keys
-
-cur.execute("DELETE FROM users_and_activities.mailchimp_sub USING users_and_activities.mailchimp_sub_staging WHERE users_and_activities.mailchimp_sub.email_address = users_and_activities.mailchimp_sub_staging.email_address")
-db.commit()
-cur.execute("INSERT INTO users_and_activities.mailchimp_sub SELECT * FROM users_and_activities.mailchimp_sub_staging")
-db.commit()
-cur.execute("DROP TABLE users_and_activities.mailchimp_sub_staging")
 db.commit()
 
 # Close Connection in case of loop failure
