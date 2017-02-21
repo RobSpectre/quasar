e4745fa8b4206a1c7d1413288bde36ff60ae302d
Sena Heydari <sheydari@dosomething.org>
2017-02-21 14:29:06 -0500

    Updated docstring blocks.

--- etl-scripts/northstar_to_user_table.py
+++ etl-scripts/northstar_to_user_table.py
@@ -5,13 +5,26 @@ import re
 import sys
 from DSNorthstarScraper import NorthstarScraper
 
-# This is to track time of script
-start = time.time()
+"""DS Northstar to Quasar User ETL script.
+
+This ETL scripts scrapes the DoSomething Northstar User API and ETL's the
+output to our MySQL Quasar data warehouse.
+
+The script takes an optional argument for what Northstar page result to start
+on. This is mostly used to backfill from a certain page, or from the dawn
+of time. Otherwise, pagination is stored in an small status tracking table
+that gets updated on ingestion loop.
+
+"""
+
+start_time = time.time()
+"""Keep track of start time of script."""
 
 ns_fetcher = NorthstarScraper()
 
 ns_member_counter = ns_fetcher.userCount()
 ns_pages = ns_member_counter[1]
+"""Get current max page number."""
 
 db = MySQLdb.connect(host=config.host, #hostname
           user=config.user, #  username
@@ -22,6 +35,7 @@ cur = db.cursor()
 cur.execute('SET NAMES utf8;')
 cur.execute('SET CHARACTER SET utf8;')
 cur.execute('SET character_set_connection=utf8;')
+"""Set UTF-8 encoding on MySQL connection."""
 
 if len(sys.argv) < 2:
     cur.execute("SELECT * from quasar_etl_status.northstar_ingestion WHERE counter_name = 'last_page_scraped';")
@@ -30,6 +44,7 @@ if len(sys.argv) < 2:
     i = last_page[0][1]
 else:
     i = int(sys.argv[1])
+"""Check if page starting point provided, otherwise use last processed point."""
 
 def ts(base_value):
     """Converts to string and replaces values with NULL when blank or None."""
@@ -59,3 +74,7 @@ while i <= ns_pages:
     i+=1
     cur.execute("REPLACE INTO quasar_etl_status.northstar_ingestion (counter_name, counter_value) VALUES(\"last_page_scraped\", \"{0}\")".format(i))
     db.commit()
+
+end_time = time.time()  # Record when script stopped running.
+duration = end_time - start_time # Total duration in seconds.
+print ('duration: ', duration)
