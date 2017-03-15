a029c601134ad4f7a7b65745a7cea244d4421027
Sena Heydari <sheydari@dosomething.org>
2017-03-15 16:05:19 -0400

    Went totally different direction and made direct DB query from Phoenix to Quasar Campaign Info table.

--- etl-scripts/phoenix_to_campaign_info_table.py
+++ etl-scripts/phoenix_to_campaign_info_table.py
@@ -2,16 +2,10 @@ import MySQLdb
 import config
 import time
 import sys
-import re
-from DSPhoenixWebScraper import PhoenixScraper
+
 
 start_time = time.time()
 """Keep track of start time of script."""
-
-phoenix_fetcher = PhoenixScraper()
-
-total_pages = phoenix_fetcher.getPages()
-page_number = 1
 
 phoenix_campaigns = phoenix_fetcher.getCampaigns()
 
@@ -26,77 +20,76 @@ cur.execute('SET CHARACTER SET utf8;')
 cur.execute('SET character_set_connection=utf8;')
 """Set UTF-8 encoding on MySQL connection."""
 
+# Query to update Quasar campaign_info table from Phoenix DB.
+update_campaign_table_query = """
+REPLACE INTO quasar.campaign_info (campaign_node_id, campaign_node_id_title,
+                                   campaign_run_id, campaign_run_id_title,
+                                   campaign_type, campaign_language,
+                                   campaign_run_start_date,
+                                   campaign_run_end_date,
+                                   campaign_created_date,
+                                   campaign_action_type, campaign_cause_type,
+                                   campaign_cta, campaign_noun, campaign_verb)
+SELECT
+  c.field_campaigns_target_id as 'campaign_node_id',
+  n2.title as 'campaign_node_id_title',
+  c.entity_id as 'campaign_run_id',
+  n1.title as 'campaign_run_id_title',
+  fdfct.field_campaign_type_value as 'campaign_type',
+  group_concat(distinct(c.language) separator ' ') as 'campaign_language',
+  fdfrd.field_run_date_value as 'campaign_run_start_date',
+  fdfrd.field_run_date_value2 as 'campaign_run_end_date',
+  from_unixtime(n1.created) as 'campaign_created_date',
+  ttd1.name as 'campaign_action_type',
+  ttd2.name as 'campaign_cause_type',
+  fdfcta.field_call_to_action_value as 'campaign_cta',
+  fdfrn.field_reportback_noun_value as 'campaign_noun',
+  fdfrv.field_reportback_verb_value as 'campaign_verb'
+FROM
+  dosomething.field_data_field_campaigns c
+LEFT JOIN
+  dosomething.node n1
+    ON n1.nid = c.entity_id
+LEFT JOIN
+  dosomething.node n2
+    ON n2.nid = c.field_campaigns_target_id
+LEFT JOIN
+  dosomething.field_data_field_campaign_type fdfct
+    ON c.field_campaigns_target_id = fdfct.entity_id
+LEFT JOIN
+  dosomething.field_data_field_run_date fdfrd
+    ON c.entity_id = fdfrd.entity_id and c.language = fdfrd.language
+LEFT JOIN
+  dosomething.field_data_field_call_to_action fdfcta
+    ON c.field_campaigns_target_id = fdfcta.entity_id and c.language = fdfcta.language
+LEFT JOIN
+  dosomething.field_data_field_reportback_noun fdfrn
+    ON c.field_campaigns_target_id = fdfrn.entity_id and c.language = fdfrn.language
+LEFT JOIN
+  dosomething.field_data_field_reportback_verb fdfrv
+    ON c.field_campaigns_target_id = fdfrv.entity_id and c.language = fdfrv.language
+LEFT JOIN
+  dosomething.field_data_field_action_type fdfat
+    ON fdfat.entity_id = c.field_campaigns_target_id
+LEFT JOIN
+  dosomething.taxonomy_term_data ttd1
+    ON fdfat.field_action_type_tid = ttd1.tid
+LEFT JOIN
+  dosomething.field_data_field_cause fdfc
+    ON fdfc.entity_id = c.field_campaigns_target_id
+LEFT JOIN
+  dosomething.taxonomy_term_data ttd2
+    ON fdfc.field_cause_tid = ttd2.tid
+WHERE c.bundle = 'campaign_run'
+GROUP BY c.entity_id;"""
 
-def null_cleanup(base_value):
-    """Replaces None in campaign field values."""
-    transform_null = {}
-    transform_null['id'] = base_value['id']
-    transform_null['title'] = base_value['title']
-    transform_null['type'] = base_value['type']
-    transform_null['language'] = base_value['language']
-    transform_null['campaign_runs'] = base_value['campaign_runs']
-    transform_null['created_at'] = base_value['created_at']
-    if base_value['action_types']['primary'] is None:
-        transform_null['action_types'] = {'primary': {'name': 'NULL'}}
-    else:
-        transform_null['action_types'] = base_value['action_types']
-    transform_null['tagline'] = base_value['tagline']
-    if base_value['reportback_info']['noun'] is None:
-        transform_null['reportback_info'] = {'verb': 'NULL', 'noun': 'NULL'}
-    else:
-        transform_null['reportback_info'] = base_value['reportback_info']
-    return transform_null
+# Run and commit new data to campaign_info Quasar table.
+cur.execute(update_campaign_table_query)
+db.commit()
 
-
-def to_string(base_value):
-    """Converts to string and replaces values with NULL when blank or None."""
-    if base_value is not None:
-        base_string = str(base_value)
-        strip_special_chars = re.sub(r'[()<>/"\'\\]', '', base_string)
-        transform_null = re.sub(r'^$|\bNone\b', 'NULL', strip_special_chars)
-        return str(transform_null)
-    else:
-        blank_value = "NULL"
-        return blank_value
-
-while page_number <= total_pages:
-    phoenix_campaigns = phoenix_fetcher.getCampaigns(page_number)
-    for i in phoenix_campaigns:
-        k = null_cleanup(i)  # Cleanup Campaign to have no empty values.
-        for j in k['campaign_runs']:
-            for lang in k['campaign_runs'][j]:
-                run_id = k['campaign_runs'][j][lang]['id']
-                query = "REPLACE INTO quasar.campaign_info \
-                         (campaign_node_id,\
-                          campaign_node_id_title,\
-                          campaign_run_id,\
-                          campaign_type,\
-                          campaign_language,\
-                          campaign_created_date,\
-                          campaign_action_type,\
-                          campaign_cta,\
-                          campaign_noun,\
-                          campaign_verb)\
-                          VALUES({0},\"{1}\",\
-                          {2},\"{3}\",\"{4}\",\
-                          from_unixtime({5}),\"{6}\",\
-                          \"{7}\", \"{8}\",\
-                          \"{9}\")".format(
-                          k['id'],
-                          to_string(k['title']),
-                          run_id,
-                          to_string(k['type']),
-                          to_string(k['language']['language_code']),
-                          k['created_at'],
-                          to_string(k['action_types']['primary']['name']),
-                          to_string(k['tagline']),
-                          to_string(k['reportback_info']['noun']),
-                          to_string(k['reportback_info']['verb'])
-                          )
-                cur.execute(query)
-                db.commit()
-
-    page_number += 1
+# Close Cursor and Connection
+cur.close()
+db.close()
 
 end_time = time.time()  # Record when script stopped running.
 duration = end_time - start_time  # Total duration in seconds.
