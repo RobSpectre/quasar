7c50265c355f489b71ebbf01bb307528c18d339a
Sena Heydari <sheydari@dosomething.org>
2017-04-05 14:46:12 -0400

    Adding ingestion script to update Northstar with missing email addresses from Mailchimp.

---
+++ etl-scripts/mailchimp_subscribers_to_northstar.py
@@ -0,0 +1,139 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import time
+from datetime import datetime
+import MySQLdb
+import MySQLdb.converters
+import sys
+import config
+from DSNorthstarScraper import NorthstarScraper
+
+
+def isInt(s):
+    """Check if value is type int and return boolean result.
+    Source at http://stackoverflow.com/questions/1265665/python-check-if-a-string-represents-an-int-without-using-try-except
+    """
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+# This is to track time of script
+start = time.time()
+
+# MailChimp API v3.0 HTTP Simple Auth Credentials
+un = config.mailchimp_api_user
+pw = config.mailchimp_api_pass
+
+conv_dict = MySQLdb.converters.conversions.copy()
+conv_dict[246] = float
+conv_dict[3] = int
+
+# Setup DB Connnection
+# Moved from earlier in script to not
+# have open MySQL connection that can timeout if
+# API scraping takes a long time. Have seen timeouts happen.
+
+ca_settings = {'ca': '/home/quasar/rds-combined-ca-bundle.pem'}
+db = MySQLdb.connect(host=config.host,  # hostname
+                     user=config.user,  # username
+                     passwd=config.pw,  # password
+                     use_unicode=True,  # Use unicode for queries.
+                     charset='utf8',    # Use UTF8 character set for queries.
+                     ssl=ca_settings,   # Connect using SSL
+                     conv=conv_dict)    # datatype conversions
+
+cur = db.cursor()
+
+if len(sys.argv) == 3:
+    if isInt(sys.argv[1]):
+        backfill_hours = int(sys.argv[1])
+        print("Backfilling by {0} hours".format(sys.argv[1]))
+    else:
+        print("Please provide number of hours to backfill.")
+        sys.exit(0)
+# Continue from last known offset or the beginning of hours.
+    if sys.argv[2] == 'cont':
+        cur.execute("SELECT * from quasar_etl_status.northstar_ingestion \
+                 WHERE counter_name = 'mailchimp_to_northstar_offset'")
+        db.commit()
+        last_offset = cur.fetchall()
+        member_offset = last_offset[0][1]
+        print("Offset is {0} members".format(member_offset))
+    elif isInt(sys.argv[2]):
+        member_offset = int(sys.argv[2])
+        print("Offset is {0} members".format(sys.argv[2]))
+    else:
+        print("Please provide valid offset.")
+        sys.exit(0)
+else:
+    print("Please provide valid arguments for backfill hours/offset.")
+    sys.exit(0)
+
+# Set environment url for Northstar, prod or thor.
+ns_fetcher = NorthstarScraper('https://northstar.dosomething.org')
+
+# Start time in Unixtime to use internally in ETL processing
+time_now = time.time()
+# Set backfill to seconds
+backfill_time = backfill_hours * 3600
+# Set backfill origin time to time_now - backfill_time in seconds
+origin_time = time_now - backfill_time
+
+# MailChimp API v3.0 Parameters to Get Subscribed Users
+# over designated time window by paginating via "offset" parameter.
+# Grabs in batches of 1000 to be nice to API. Can modify based on query times.
+info = {'status': 'subscribed',
+        'since_timestamp_opt': datetime.utcfromtimestamp(origin_time).isoformat(),
+        'before_timestamp_opt': datetime.utcfromtimestamp(time_now).isoformat(),
+        'count': 1000, 'fields': 'members.email_address,members.timestamp_opt,members.status,members.stats,members.list_id,members.location',
+        'offset': member_offset}
+# Initialize Empty List for Total Members
+total_members = []
+
+
+# Get first batch of users
+r = requests.get('https://us4.api.mailchimp.com/3.0/lists/f2fab1dfd4/members',
+                 auth=HTTPBasicAuth(un, pw), params=info)
+member_array = r.json()
+
+# Iterate over entire list set till no more members are returned.
+while (len(member_array['members'])) > 1:
+    total_members.append(member_array['members'])
+    for member in total_members:
+        print("Processing next page. Up to %s processed." % member_offset)
+        for submember in member:
+            cur.execute("SELECT northstar_id FROM quasar.users \
+                        WHERE email = %s", (submember['email_address'],))
+            northstar_email_matches = cur.rowcount
+            if northstar_email_matches < 1:
+                ns_fetcher.post('/v1/users', {'email': submember['email_address']})
+            else:
+                pass
+        total_members = []
+    member_offset += 1000
+    cur.execute("REPLACE INTO quasar_etl_status.northstar_ingestion \
+                 (counter_name, counter_value) VALUES(\"mailchimp_to_northstar_offset\",\
+                 \"{0}\")".format(member_offset))
+    db.commit()
+    info = {'status': 'subscribed',
+            'since_timestamp_opt': datetime.utcfromtimestamp(origin_time).isoformat(),
+            'before_timestamp_opt': datetime.utcfromtimestamp(time_now).isoformat(),
+            'count': 1000,
+            'fields': 'members.email_address,members.timestamp_opt,members.status,members.stats,members.list_id,members.location',
+            'offset': member_offset}
+    r = requests.get('https://us4.api.mailchimp.com/3.0/lists/f2fab1dfd4/members',
+                     auth=HTTPBasicAuth(un, pw), params=info)
+    member_array = r.json()
+
+
+# Commit DB Changes and Close Connection in case of loop failure
+cur.close()
+db.close()
+
+# Print How Long Run Took
+end = time.time()
+timer = end-start
+print("Total processing time was %s seconds." % timer)
