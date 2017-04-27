45b9b3a863f6b01cb5af181ce5f5a42b99d5aab5
Rob Spectre <rob@brooklynhacker.com>
2017-04-27 16:46:48 -0400

    Added logging.

--- etl-scripts/test-blink-queue.py
+++ etl-scripts/test-blink-queue.py
@@ -1,6 +1,10 @@
 from QuasarQueue import QuasarQueue
+import logging
 
 
 if __name__ == "__main__":
+    log_format = "%(asctime)s - %(levelname)s: %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_format)
+
     testQuasarQueue = QuasarQueue()
     testQuasarQueue.start()
